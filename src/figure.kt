526929e04dded118c4c87cca84e728258ea7b550
KonstBeliakov <konstbeliakov@gmail.com>
2025-01-21 00:50:40 +0300

    Different colors for different types of figures

--- src/figure.kt
+++ src/figure.kt
@@ -35,33 +35,6 @@ val blockPositions = listOf(
 
     listOf(
         listOf(
-            listOf(1, 1, 0, 0),
-            listOf(1, 1, 0, 0),
-            listOf(0, 0, 0, 0),
-            listOf(0, 0, 0, 0)
-        ),
-        listOf(
-            listOf(1, 1, 0, 0),
-            listOf(1, 1, 0, 0),
-            listOf(0, 0, 0, 0),
-            listOf(0, 0, 0, 0)
-        ),
-        listOf(
-            listOf(1, 1, 0, 0),
-            listOf(1, 1, 0, 0),
-            listOf(0, 0, 0, 0),
-            listOf(0, 0, 0, 0)
-        ),
-        listOf(
-            listOf(1, 1, 0, 0),
-            listOf(1, 1, 0, 0),
-            listOf(0, 0, 0, 0),
-            listOf(0, 0, 0, 0)
-        )
-    ),
-
-    listOf(
-        listOf(
             listOf(0, 0, 1, 0),
             listOf(1, 1, 1, 0),
             listOf(0, 0, 0, 0),
@@ -110,6 +83,33 @@ val blockPositions = listOf(
             listOf(0, 1, 0, 0),
             listOf(0, 1, 0, 0),
             listOf(1, 1, 0, 0),
+            listOf(0, 0, 0, 0)
+        )
+    ),
+
+    listOf(
+        listOf(
+            listOf(1, 1, 0, 0),
+            listOf(1, 1, 0, 0),
+            listOf(0, 0, 0, 0),
+            listOf(0, 0, 0, 0)
+        ),
+        listOf(
+            listOf(1, 1, 0, 0),
+            listOf(1, 1, 0, 0),
+            listOf(0, 0, 0, 0),
+            listOf(0, 0, 0, 0)
+        ),
+        listOf(
+            listOf(1, 1, 0, 0),
+            listOf(1, 1, 0, 0),
+            listOf(0, 0, 0, 0),
+            listOf(0, 0, 0, 0)
+        ),
+        listOf(
+            listOf(1, 1, 0, 0),
+            listOf(1, 1, 0, 0),
+            listOf(0, 0, 0, 0),
             listOf(0, 0, 0, 0)
         )
     ),
@@ -196,13 +196,16 @@ val blockPositions = listOf(
     ),
 )
 
+val figureColors =
+    listOf(Colors.GREEN, Colors.ORANGE, Colors.YELLOW, Colors.PINK, Colors.BLUE, Colors.GREEN, Colors.ORANGE)
+
 class Figure(container: Container) {
     var x = 0
     var y = 0
-    val color = Colors.BLUE
-    val block_type = Random.nextInt(0, blockPositions.size)
+    val blockType = Random.nextInt(0, blockPositions.size)
+    val color = figureColors[this.blockType]
     var rotation = 0
-    var blockPosition = blockPositions[this.block_type][this.rotation]
+    var blockPosition = blockPositions[this.blockType][this.rotation]
     private val blocks = List(4) { i ->
         container.solidRect(BLOCK_SIZE, BLOCK_SIZE, this.color) {
             position(50.0 + i * (BLOCK_SIZE + SEPARATION), 50.0 + 0 * (BLOCK_SIZE + SEPARATION))
@@ -240,21 +243,21 @@ class Figure(container: Container) {
         }
     }
 
-    fun rotate(){
+    fun rotate() {
         this.rotation = (this.rotation + 1) % 4
-        this.blockPosition = blockPositions[this.block_type][this.rotation]
+        this.blockPosition = blockPositions[this.blockType][this.rotation]
         this.setBlocksPosition()
     }
 
-    private fun setBlocksPosition(){
+    private fun setBlocksPosition() {
         val pos: MutableList<List<Int>> = mutableListOf()
-        for(i in 0..<4){
-            for(j in 0..<4) {
-                if(this.blockPosition[i][j] == 1)
+        for (i in 0..<4) {
+            for (j in 0..<4) {
+                if (this.blockPosition[i][j] == 1)
                     pos.add(listOf(i, j))
             }
         }
-        for(i in 0..<4){
+        for (i in 0..<4) {
             blocks[i].x = 50.0 + (this.x + pos[i][0]) * (BLOCK_SIZE + SEPARATION)
             blocks[i].y = 50.0 + (this.y + pos[i][1]) * (BLOCK_SIZE + SEPARATION)
         }
